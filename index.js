3bbf075cc0e7dbe35b106de61ed0a316f864f55a
Gwangz <bnyagwanga@gmail.com>
2022-09-25 21:36:26 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require ("http");
+const fs = require("fs");
+const httpServer = http.createServer();
+httpServer.on("Listening", () => console.log("Listening..."));
+httpServer.on("request", (req, res) =>{
+
+    if (req.url === "/")
+    res.end(fs.readFileSync("index.html"));
+
+    if (req.url === "/upload"){
+        const fileName = req.headers["file-name"];
+        req.on("data", chunk =>{
+            fs.appendFileSync(fileName, chunk)
+            console.log(`received chunk! ${chunk.length}`)
+        })
+        res.end("uploaded!")
+    }
+    return
+})
+
+
+httpServer.listen(8080)
